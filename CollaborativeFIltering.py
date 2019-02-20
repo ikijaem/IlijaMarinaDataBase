1039f359a69a74d829c6b1edfa8fdbd787dc0de5
ikijaem <ilija.malinovic@yahoo.com>
2019-02-20 19:49:39 +0100

    collaborative

---
+++ CollaborativeFIltering.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+class CollaborativeFiltering:
+
+    def __init__(self, ratings, movies):
+        self.ratings = ratings
+        self.movies=movies
+
+        n_users = ratings.user_id.unique().shape[0]
+        n_movies = ratings.movie_id.unique().shape[0]
+
+        self.data_matrix = np.zeros((n_users, n_movies))
+        for line in ratings.itertuples():
+            self.data_matrix[line[1] - 1, line[2] - 1] = line[3]
+
+        self.user_similarity = cosine_similarity(self.data_matrix, self.data_matrix)
+        self.item_similarity = cosine_similarity(self.data_matrix.T, self.data_matrix.T)
+
+
+    def getMoviesUserRated(self, user):
+        self.ratings = self.ratings.sort_values(by='user_id')
+        user_ratings = self.ratings.loc[self.ratings['user_id'] == user]
+        #return user_ratings
+
+        return list(user_ratings['movie_id'])
+
+    def getPredictionMatrix(self, type):
+        if (type == 'user') :
+            similarity = self.user_similarity
+            predictions_matrix = similarity.dot(self.data_matrix)
+        else:
+            similarity = self.item_similarity
+            predictions_matrix = self.data_matrix.dot(similarity)
+
+        #dec because each user is "similar" to himself
+        sum_similarities= list([np.abs(similarity).sum(axis=1)])
+        for i in range(0, len(sum_similarities[0])):
+            sum_similarities[0][i] = sum_similarities[0][i]-1
+
+        for i in range(0, len(predictions_matrix)):
+            for j in range(0, len(predictions_matrix[0])):
+                predictions_matrix[i, j] = predictions_matrix[i, j] / sum_similarities[0][i]
+
+        return predictions_matrix
+
+    def findRecommendations(self, user, type):
+        predictions_matrix = self.getPredictionMatrix(type)
+        predictions = pd.Series(predictions_matrix[user]).sort_values(ascending=False)
+
+        movies_user_rated = self.getMoviesUserRated(user)
+        # get top 10 results
+        recommended_movies = []
+        for i in list(predictions.index):
+            if(i+1 in movies_user_rated):
+                continue;
+            recommended_movies.append(self.movies.at[i, 'movie_title'])
+            if (len(recommended_movies) == 10):
+                break;
+
+        return recommended_movies
--- IlijaMarinaDataBase.py
+++ IlijaMarinaDataBase.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 from sklearn.metrics.pairwise import pairwise_distances, cosine_similarity
 from ContentBasedFiltering import ContentBasedFiltering
+from CollaborativeFIltering import CollaborativeFiltering
 
 
 #variables containg data
@@ -35,39 +36,11 @@ def randomizeMovieLikedByUser(ratings, user):
     movie_idx = random.randint(0, len(movs)-1)
     return movs[movie_idx]
 
-def getReccomendationsCollaborative(ratings, movies):
-    n_users = ratings.user_id.unique().shape[0]
-    n_movies = ratings.movie_id.unique().shape[0]
-    data_matrix = np.zeros((n_users, n_movies))
-    for line in ratings.itertuples():
-        data_matrix[line[1] - 1, line[2] - 1] = line[3]
-
-    similarity = cosine_similarity(data_matrix, data_matrix)
-
-    wh = np.array([np.abs(similarity).sum(axis=1)]).T
-
-    what = similarity.dot(data_matrix)/wh
-
-
-
-    predictions = pd.Series(what[0]).sort_values(ascending=False)
-
-
-    # get top 10 results
-    recommended_movies = []
-    for i in list(predictions.index):
-        recommended_movies.append(movies.at[i, 'movie_title'])
-        if (len(recommended_movies) == 10):
-            break;
-
-
 if __name__ == '__main__':
     users, ratings, movies = loadData();
 
     cbf = ContentBasedFiltering(movies)
-
-
-    #content based
+    cf = CollaborativeFiltering(ratings, movies)
 
     #naredne 4 linije, random biraju film za nekog random usera, i onda na osnovu tog filma ce mu preporuciti
     user_id = randomizeUser(users)
@@ -75,13 +48,9 @@ if __name__ == '__main__':
     movie_id = randomizeMovieLikedByUser(ratings, user_id)
     print(movie_id)
 
-    print (cbf.findRecommendations(movie_id))
+    #content based
+    #print (cbf.findRecommendations(movie_id))
 
-
-    #print(ratings.sort_values(by=['user_id']))
-    #print (getReccomendationsCollaborative(ratings, movies))
-
-
-
-
+    #collaborative
+    print (cf.findRecommendations(603, 'user'))
 
